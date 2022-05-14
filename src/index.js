10f2522b944e2064da978548847412514ec70a28
Mornieur <mariafernanda-coro@hotmail.com>
2022-05-14 15:03:39 -0300

    Tipos de parametros

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 /**
  * GET - Buscar uma informação dentro do servidor.
  * POST - Inserir uma informação no servidor.
@@ -10,15 +12,27 @@ const app = express();
  * DELETE - Deletar uma informação no servidor.
  */
 
+/**
+ * Route Params => Indentificar um recurso editar/deletar/buscar
+ * Query Params => Paginação / Filtro
+ * Body Params => Os objetos inserção/alteração (JSON)
+ */
+
 app.get("/courses", (request, response) => {
+  const query = request.query;
+  console.log(query);
   response.json(["Curso 1", "Curso 2", "Curso 3"]);
 });
 
 app.post("/courses", (request, response) => {
+  const body = request.body;
+  console.log(body);
   response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
 });
 
 app.put("/courses/:id", (request, response) => {
+  const { id } = request.params;
+  console.log(id);
   response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
 });
 
